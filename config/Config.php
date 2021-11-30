7b02a8713eb6f14718c2b19f02a16655a40016fa
LeRouteur <47207700+lerouteur@users.noreply.github.com>
2021-11-30 15:42:45 +0100

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -1,4 +1,7 @@
 <?php
 
-define("UI_URL", "http://dev.ges.ui.ch/");
-define("API_URL", "http://dev.ges.api.ch/");
+define("UI_URL", "https://ges.storagehost.ch/");
+define("API_URL", "https://api.ges.storagehost.ch/");
+
+//define("UI_URL", "http://dev.ges.ui.ch/");
+//define("API_URL", "http://dev.ges.api.ch/");
